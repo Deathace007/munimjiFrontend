7e3a1411f6440b575049f18bd1220fcc96336c14
Deathace007 <mohanborle17@gmail.com>
2025-05-09 21:29:51 +0530

    add photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormComponent],
+  imports: [RouterOutlet, FormComponent, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <app-header />
+    <app-form />
 
-    <router-outlet />
+
   `,
   styles: [],
 })
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -15,13 +15,92 @@ export class FormComponent {
   constructor(private fb: FormBuilder) {
     this.myForm = this.fb.group({
       name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]]
+      entity: ['', Validators.required],
     });
+  }
+
+  onView() {
+    console.log('View Button Clicked:', this.myForm.value);
+  }
+
+  onUpdate() {
+    if (this.myForm.valid) {
+      console.log('Update Button Clicked:', this.myForm.value);
+    }
   }
 
   onSubmit() {
     if (this.myForm.valid) {
-      console.log('Form Submitted:', this.myForm.value);
+      console.log('Submit Button Clicked:', this.myForm.value);
     }
+  }
+}
+
+@Component({
+  selector: 'app-form-a',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './form-a.component.html',
+  styleUrls: ['./form-a.component.css']
+})
+export class FormAComponent {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      fieldA: ['', Validators.required],
+      fieldB: ['', Validators.required],
+    });
+  }
+
+  onSave() {
+    if (this.myForm.valid) {
+      console.log('Save Button Clicked:', this.myForm.value);
+    }
+  }
+}
+
+@Component({
+  selector: 'app-form-b',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './form-b.component.html',
+  styleUrls: ['./form-b.component.css']
+})
+export class FormBComponent {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      fieldX: ['', Validators.required],
+      fieldY: ['', Validators.required],
+    });
+  }
+
+  onDelete() {
+    console.log('Delete Button Clicked:', this.myForm.value);
+  }
+}
+
+@Component({
+  selector: 'app-form-c',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './form-c.component.html',
+  styleUrls: ['./form-c.component.css']
+})
+export class FormCComponent {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      fieldM: ['', Validators.required],
+      fieldN: ['', Validators.required],
+    });
+  }
+
+  onReset() {
+    this.myForm.reset();
+    console.log('Reset Button Clicked');
   }
 }
